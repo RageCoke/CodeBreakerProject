c5133632d5bfba0b225c21abe7f9d228f8c9690f
George <ragecoke1466@outlook.com>
2017-05-19 21:34:22 +0800

    task5

--- src/assets/main.js
+++ src/assets/main.js
@@ -8,5 +8,11 @@ function guess() {
 
 //implement new functions here
 function setHiddenFields(){
-  answer = Math.floor(Math.random() * 10000).toString();
+  if(answer == "") {
+      answer = Math.floor(Math.random() * 10000).toString();
+      while(answer.length < 4) {
+          answer = "0" + answer;
+      }
+      document.getElementById('answer').value = answer;
+  }
 }
